28b9a9af60ddf82a34c0ff8fde456f7b8fbbfb6a
Kaili Nishihira <kailinishihira@gmail.com>
2017-10-09 13:31:44 -0700

    final commit with css styling

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -54,6 +54,8 @@ namespace ToDoList
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
