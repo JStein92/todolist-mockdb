754c636fb39eaa44f3bde314291cffc8ab17c905
Kaili Nishihira <kailinishihira@gmail.com>
2017-10-09 10:26:09 -0700

    break commit

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -7,17 +7,33 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
 
 namespace ToDoList
 {
     public class Startup
     {
+        public IConfiguretionRoot Configuration { get; set; }
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             //1
             services.AddMvc();
+            services.AddEntityFrameworkMySql()
+                    .AddDbContext<ToDoList>(options =>
+                                            options
+                                            .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,7 +42,7 @@ namespace ToDoList
             app.UseMvc(routes =>
                {
                    routes.MapRoute(
-                       nameof: "default",
+                       name: "default",
                        template: "{controller=Home}/{action=Index}/{id?}");
                });
 
